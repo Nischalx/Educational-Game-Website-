5cc499c5bd9a7043a1c60b862a9db70d8f887274
Nischalx <92905757+Nischalx@users.noreply.github.com>
2024-09-14 08:35:31 +0530

    Add files via upload

---
+++ education web/Word Guessing Game/Word Guessing Game/js/words.js
@@ -0,0 +1,37 @@
+let wordList = [
+    {
+        word: "html",
+        hint: "programming language"
+    },
+    {
+        word: "head",
+        hint: "tag used to define the head section of an HTML document"
+    },
+    {
+        word: "backend develpoment",
+        hint: "refers to the server-side coding"
+    },
+    {
+        word: "browser",
+        hint: " allows  users and developers to view webpages "
+    },
+    {
+        word: "CSS",
+        hint: " responsible for the visual elements of a website"
+    },
+    {
+        word: "favicon",
+        hint: "A small icon image, often a company logo"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+   
+   
+
+]
---
+++ education web/Word Scramble Game/Word Scramble Game/js/words.js
@@ -0,0 +1,26 @@
+let words = [
+    {
+        word: "HTML",
+        hint: "Markup language for creating web pages"
+    },
+    {
+        word: "event",
+        hint: "An action or occurrence on a web page, often responded to by JavaScript"
+    },
+    {
+        word: "variable",
+        hint: "A container for storing data in programming"
+    },
+    {
+        word: "API",
+        hint: "A set of rules for building software applications, often used for web development"
+    },
+    {
+        word: "DOM",
+        hint: "Document Object Model, a programming interface for web documents"
+    },
+    {
+        word: "function",
+        hint: "A reusable block of code, often associated with a specific task"
+    },
+];
